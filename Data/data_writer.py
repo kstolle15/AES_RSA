4fecf112d69100458482a2c378e197dc23e393ac
kstolle15 <stollekyle@gmail.com>
2020-04-21 00:18:54 -0500

    Added imports to data_writer.py and created testData1.txt

--- Data/data_writer.py
+++ Data/data_writer.py
@@ -1,1 +1,8 @@
+# Kyle Stolle - CSCE 463 Final Project: AES vs RSA 04/21/2020
 # This program will be used to create test data for the AES and RSA algorithms
+
+import os 
+import string 
+
+alphabet = list(string.ascii_lowercase)
+print(alphabet)
