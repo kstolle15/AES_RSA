65dc0bcd5bdc4ca1b4e2d29a7222f041a4eda2b7
Kyle Stolle <kyle.stolle@huskers.unl.edu>
2020-04-26 15:39:36 -0500

    Added aes class and some corresponding functions

--- Crypto/AES.py
+++ Crypto/AES.py
@@ -1,3 +1,27 @@
 # Kyle Stolle - CSCE 463 Final Projecct: AES vs RSA 04/26/2020
-# This program will be used to encrypt and decrypt data using AES
+# This program will be used to encrypt and decrypt data using AES-CBC
 
+from Crypto.Cipher import AES
+from Crypto.Util import Padding
+
+class aes:
+    def __init__(self):
+        self.key = setKey()
+        self.iv = setIV()
+
+    def setKey():
+        key = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
+        return key
+
+    def setIV():
+        iv = ''.join(chr(random.randint(0,0xFF)) for i in range(16))
+    
+    def getKey():
+        return self.key
+    
+    def getIV():
+        return self.iv
+
+    def encrypt(plaintext):
+        enc = AES.new(self.key,AES.MODE_CBC,self.iv)
+        return enc.encrypt(plaintext.encode())
