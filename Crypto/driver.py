d20305706b1c5d0f2b44230560d3892ed3d9491a
Kyle Stolle <kyle.stolle@huskers.unl.edu>
2020-04-28 17:58:39 -0500

    Added total time for both algorithms

--- Crypto/driver.py
+++ Crypto/driver.py
@@ -6,6 +6,8 @@ import rsa
 import data_reader as dr 
 import data_writer as dw 
 from Crypto.Util.Padding import pad, unpad
+from Crypto.Cipher import PKCS1_OAEP
+import datetime
 
 def runAES(small,med,large,five):
     crypt = aes()
@@ -21,71 +23,51 @@ def runAES(small,med,large,five):
 def runRSA(small,med,large):
     # small 
     rsaSmall(small)
-    print()
     rsaMed(med)
-    print()
     rsaLarge(large)
 
 def rsaSmall(small):
     pksmall = rsa.getPubKey1024()
     sksmall = rsa.getSecretKey1024()
     print("RSA Encrypting up to 117-byte strings")
-    cipher = rsa.encryptPhrases(small,pksmall)
-    print("RSA Decrypting up to 117-byte strings")
-    recoverd = rsa.decryptPhrases(cipher,sksmall)
-    print("RSA Done with Encryption and Decryption of 117-byte strings")
+    cipher = rsaEncryptPhrases(small,pksmall)
+    recoverd = rsaDecryptPhrases(cipher,sksmall)
 
 def rsaMed(med):
     pkmed = rsa.getPubKey2048()
     skmed = rsa.getSecretKey2048()
     print("RSA Encrypting up to 245-byte strings")
-    cipher = rsa.encryptPhrases(med,pkmed)
-    print("RSA Decrypting up to 245-byte strings")
-    recoverd = rsa.decryptPhrases(cipher,skmed)
-    print("RSA Done with Encryption and Decryption of 245-byte strings")
+    cipher = rsaEncryptPhrases(med,pkmed)
+    recoverd = rsaDecryptPhrases(cipher,skmed)
 
 def rsaLarge(large):
     pklarge = rsa.getPubKey4096()
     sklarge = rsa.getSecretKey4096()
     print("RSA Encrypting up to 468-byte strings")
-    cipher = rsa.encryptPhrases(large,pklarge)
-    print("RSA Decrypting up to 468-byte strings")
-    recoverd = rsa.decryptPhrases(cipher,sklarge)
-    print("RSA Done with Encryption and Decryption of 468-byte strings")
+    cipher = rsaEncryptPhrases(large,pklarge)
+    recoverd = rsaDecryptPhrases(cipher,sklarge)
 
 def aesSmall(small,crypt):
     print("AES Encrypting up to 117-byte Strings")
-    sCiphers = encryptPhrases(small,crypt)
-    print("AES Decrypting up to 117-byte strings")
-    sRecovered = decryptPhrases(sCiphers,crypt)
-    print("AES Done with Encryption and Decryption of 117-byte strings")
-    print(small[3],sCiphers[3],sRecovered[3])
+    sCiphers = aesEncryptPhrases(small,crypt)
+    sRecovered = aesDecryptPhrases(sCiphers,crypt)
 
 def aesMed(med,crypt):
     print("AES Encrypting up to 245-byte Strings")
-    mCiphers = encryptPhrases(med,crypt)
-    print("AES Decrypting up to 245-byte strings")
-    mRecovered = decryptPhrases(mCiphers,crypt)
-    print("AES Done with Encryption and Decryption of 245-byte strings")
-    print(med[3],mCiphers[3],mRecovered[3])
+    mCiphers = aesEncryptPhrases(med,crypt)
+    mRecovered = aesDecryptPhrases(mCiphers,crypt)
 
 def aesLarge(large,crypt):
     print("AES Encrypting up to 468-byte Strings")
-    lCiphers = encryptPhrases(large,crypt)
-    print("AES Decrypting up to 468-byte strings")
-    lRecovered = decryptPhrases(lCiphers,crypt)
-    print("AES Done with Encryption and Decryption of 468-byte strings")
-    print(large[3],lCiphers[3],lRecovered[3])
+    lCiphers = aesEncryptPhrases(large,crypt)
+    lRecovered = aesDecryptPhrases(lCiphers,crypt)
 
 def aesFiveThousand(five,crypt):
     print("AES Encrypting strings up to 5,000")
-    ciphers = encryptPhrases(five,crypt)
-    print("AES Decrypting up to 468-byte strings")
-    recoverd = decryptPhrases(ciphers,crypt)
-    print("AES Done with Encryption and Decryption of 5000 strings with max string length = 5000")
-    print(five[5],ciphers[5],recoverd[5])
+    ciphers = aesEncryptPhrases(five,crypt)
+    recoverd = aesDecryptPhrases(ciphers,crypt)
     
-def encryptPhrases(phrases,c):
+def aesEncryptPhrases(phrases,c):
     encryptedPhrases = []
     for phrase in phrases:
         plaintext = pad(phrase,16)
@@ -94,7 +76,7 @@ def encryptPhrases(phrases,c):
 
     return encryptedPhrases
 
-def decryptPhrases(phrases,c):
+def aesDecryptPhrases(phrases,c):
     decryptedPhrases = []
     for line in phrases:
         recoveredText = c.decrypt(line)
@@ -102,6 +84,23 @@ def decryptPhrases(phrases,c):
         decryptedPhrases.append(recoveredText)
 
     return decryptedPhrases
+
+def rsaEncryptPhrases(phrases,key):
+    cipher = []
+    e = PKCS1_OAEP.new(key)
+    for line in phrases:
+        ciphertext = e.encrypt(line)
+        cipher.append(ciphertext)
+    return cipher
+
+def rsaDecryptPhrases(phrases,key):
+    recover = []
+    d = PKCS1_OAEP.new(key)
+    for line in phrases:
+        recoveredtext = d.decrypt(line)
+        recover.append(recoveredtext)
+    
+    return recover
 
 # writing
 dw.write117bytes()
@@ -122,7 +121,15 @@ rsaSmallTimes = []
 rsaMedTimes = [] 
 rsaLargeTimes = []
 
+aesStart = datetime.datetime.now()
 runAES(small,med,large,fiveThousand)
+aesEnd = datetime.datetime.now()
+aesTotal = aesEnd - aesStart
+print(str(aesTotal.seconds) + "." + str(aesTotal.microseconds) + " seconds")
 print()
 print()
+rsaStart = datetime.datetime.now()
 runRSA(small,med,large)
+rsaEnd = datetime.datetime.now()
+totalRSA = rsaEnd - rsaStart
+print(str(totalRSA.seconds) + "." + str(totalRSA.microseconds) + " seconds")
