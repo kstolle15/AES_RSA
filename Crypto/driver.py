21d82862aae8a208c42bb2857bc2245fc6d448bd
kstolle15 <stollekyle@gmail.com>
2020-05-07 13:43:27 -0500

    Finished RSA testing for driver

--- Crypto/driver.py
+++ Crypto/driver.py
@@ -86,6 +86,7 @@ def runRSA(s,m,l):
     rsaDecryptionAverages[0] += sAvgD
     rsaDecryptionAverages[1] += mAvgD
     rsaDecryptionAverages[2] += lAvgD
+    crypt.totalTimes = crypt.converToSeconds(crypt.totalTimes)
     for i in range(0,3):
         rsaTotals[i] += crypt.totalTimes[i]
 
